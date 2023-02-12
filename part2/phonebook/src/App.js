c3d153a785abfbbcb1168b98c25a772f84cc1cfe
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-12 17:56:48 +0100

    Implement phonebook part 11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import personsService from './services/persons'
+import Notification from "./components/Notification";
 
 const App = () => {
     const personsState = useState([]);
@@ -20,7 +21,11 @@ const App = () => {
     const searchMaskState = useState(defaultSearchMask);
     const [searchMask,] = searchMaskState
 
+    const [infoMessage, setInfoMessage] = useState(null)
+
     const searchedPersons = persons.filter(p => p.name.toLowerCase().includes(searchMask))
+
+    const removeMessage = (duration) => setTimeout(() => setInfoMessage(null), duration)
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -29,10 +34,14 @@ const App = () => {
             if (window.confirm(`${info.name} is already added to phonebook, replace the old number with a new one?`)) {
                 personsService.update(personIndex, {...info})
                     .then(updatedPerson => setPersons(persons.map(p => updatedPerson.id === p.id ? updatedPerson : p)))
+                setInfoMessage(`Updated ${info.name}`)
+                removeMessage(5000)
             }
         } else {
             const newPerson = {...info}
             personsService.create(newPerson).then(p => setPersons(persons.concat(p)))
+            setInfoMessage(`Added ${info.name}`)
+            removeMessage(5000)
         }
         setInfo(defaultInfo)
     }
@@ -40,6 +49,7 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={infoMessage}/>
             <Filter searchState={searchMaskState}/>
             <h3>Add a new</h3>
             <PersonForm properties={infoState} onSubmit={addPerson}/>
