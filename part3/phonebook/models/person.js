139b6a7cb86ddeb25dafb943a165d9700e03c9b6
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-16 16:39:44 +0100

    Integrate lint

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -6,28 +6,28 @@ mongo.set('strictQuery', false)
 mongo.connect(url)
 
 const schema = new mongo.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (value) => /\d{2,3}-\d+/.test(value)
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: (value) => /\d{2,3}-\d+/.test(value)
-        },
-        required: true
-    },
-    date: {type: Date, default: Date.now}
+    required: true
+  },
+  date: { type: Date, default: Date.now }
 })
 
 schema.set('toJSON', {
-    transform: (doc, obj) => {
-        obj.id = obj._id.toString()
-        delete obj._id
-        delete obj.__v
-    }
+  transform: (doc, obj) => {
+    obj.id = obj._id.toString()
+    delete obj._id
+    delete obj.__v
+  }
 })
 
 const Person = mongo.model('Person', schema)
