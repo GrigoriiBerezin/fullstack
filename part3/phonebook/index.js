19ab6b34faf9926bbba6c45355d326f17e0715a6
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-16 17:34:52 +0100

    Update backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -39,18 +39,12 @@ app.put('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  Person.findOne({ name: body.name }).then(isExist => {
-    if (isExist) {
-      return response.status(409).json({ error: `Name ${body.name} is already in list` })
-    }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    })
-
-    person.save().then(savedPerson => response.json(savedPerson)).catch(error => next(error))
-  }).catch(error => next(error))
+  person.save().then(savedPerson => response.json(savedPerson)).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
