601f7cf0a27e4ac43834df8bc4580062111a3e88
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-17 11:46:31 +0100

    Implement blog tests part 4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.length === 0 ? 0 : blogs.map(b => b.likes).reduce((s, l) => s + l)
@@ -10,4 +12,16 @@ const favoriteBlog = (blogs) => {
   }
 }
 
-module.exports = {dummy, totalLikes, favoriteBlog}
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  const groupByAuthor = _.groupBy(blogs, 'author')
+  const authorToBlogCount = _.map(groupByAuthor, (blogs, author) => {
+    return {author: author, blogs: blogs.length}
+  })
+  return _.maxBy(authorToBlogCount, 'blogs')
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog, mostBlogs}
