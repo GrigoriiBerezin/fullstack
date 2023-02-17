8cb111688e51792babc7433bd78a9b5ef55d6336
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-17 10:55:31 +0100

    Implement blog tests part 3

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -1,10 +1,27 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+const blogs = [
+  {
+    title: 'First article',
+    author: 'Grigorii',
+    url: 'localhost',
+    likes: 15
+  },
+  {
+    title: 'Another article',
+    author: 'Grigorii',
+    url: 'localhost:3031',
+    likes: 3
+  }
+]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('dummy', () => {
+  test('returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
 })
 
 describe('total likes', () => {
@@ -14,22 +31,28 @@ describe('total likes', () => {
   })
 
   test('return sum of likes from all blogs', () => {
-    const blogs = [
-      {
-        title: 'First article',
-        author: 'Grigorii',
-        url: 'localhost',
-        likes: 15
-      },
-      {
-        title: 'Another article',
-        author: 'Grigorii',
-        url: 'localhost:3031',
-        likes: 3
-      }
-    ]
-
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(18)
+  })
+})
+
+describe('favorite blog', () => {
+  test('return empty object on empty array', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
+  })
+
+  test('return the most liked blog', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(blogs[0])
+  })
+
+  test('return first most liked blog if few exist', () => {
+    const equalBlogs = blogs.map(b => {
+      return {...b, likes: 5}
+    })
+
+    const result = listHelper.favoriteBlog(equalBlogs)
+    expect(result).toEqual(equalBlogs[0])
   })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.length === 0 ? 0 : blogs.map(b => b.likes).reduce((s, l) => s + l)
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    return blogs.reduce((fav, cur) => (cur.likes > fav.likes) ? cur : fav)
+  }
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog}
