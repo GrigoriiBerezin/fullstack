f44a7663d214438d1bc044d8fd73ed68c4622730
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-19 07:46:48 +0100

    Implement time limit for the token

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -15,10 +15,10 @@ loginRouter.post('/', async (request, response) => {
 
   const userForToken = {
     username: user.username,
-    id: user._id
+    id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 })
 
   response.status(200).json({ token, username: user.username, name: user.name })
 })
