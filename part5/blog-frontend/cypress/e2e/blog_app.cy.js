2e21d59ddd8f9d73d492f3f3f07c878722c00046
Grigorii Berezin <grigorii_berezin@epam.com>
2023-02-23 17:41:04 +0100

    Implement bloglist e2e testing step 2

--- part5/blog-frontend/cypress/e2e/blog_app.cy.js
+++ part5/blog-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,7 @@
 describe('Blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        cy.request('POST', 'http://localhost:3001/api/users', { username: 'ping', name: 'Grigorii Berezin', password: 'terrano' })
         cy.visit('http://localhost:3000')
     })
 
@@ -9,5 +10,26 @@ describe('Blog app', function () {
         cy.get('#username')
         cy.get('#password')
         cy.contains('login')
+    })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('ping')
+            cy.get('#password').type('terrano')
+            cy.contains('login').click()
+
+            cy.contains('Grigorii Berezin logged in')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('ping')
+            cy.get('#password').type('wrong')
+            cy.contains('login').click()
+
+            cy.get('.error')
+                .should('contain', 'invalid username or password')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+                .and('have.css', 'border-style', 'solid')
+        })
     })
 })
