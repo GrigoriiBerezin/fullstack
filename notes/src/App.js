dc82ab4095e56d40e80a4c4bd268960e09511881
Grigorii Berezin <grigorii_berezin@epam.com>
2023-03-02 14:16:43 +0100

    Implement react-query on note

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,22 +1,54 @@
-import NewNote from './components/NewNote'
-import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { initializeNotes } from './reducers/noteReducer'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { createNote, getNotes, updateNote } from './requests'
 
 const App = () => {
-    const dispatch = useDispatch()
+    const queryClient = useQueryClient()
+    const newNoteMutation = useMutation(createNote, {
+        onSuccess: (newNote) => {
+            const notes = queryClient.getQueryData('notes')
+            queryClient.setQueryData('notes', notes.concat(newNote))
+        }
+    })
+    const updateNoteMutation = useMutation(updateNote, {
+        onSuccess: (updatedNote) => {
+            const notes = queryClient.getQueryData('notes')
+            queryClient.setQueryData('notes', notes.map(n => n.id === updatedNote.id ? updatedNote : n))
+        }
+    })
 
-    useEffect(() => {
-        dispatch(initializeNotes())
-    }, [dispatch])
+    const result = useQuery('notes', getNotes, {
+        refetchOnWindowFocus: false
+    })
 
-    return (
+    if (result.isLoading) {
+        return <div>loading data...</div>
+    }
+    const notes = result.data
+
+    const addNote = async (event) => {
+        event.preventDefault()
+        const content = event.target.note.value
+        event.target.note.value = ''
+        newNoteMutation.mutate({ content, important: true })
+    }
+
+    const toggleImportance = (note) => {
+        updateNoteMutation.mutate({ ...note, important: !note.important })
+    }
+
+    return(
         <div>
-            <NewNote />
-            <VisibilityFilter />
-            <Notes />
+            <h2>Notes app</h2>
+            <form onSubmit={addNote}>
+                <input name="note" />
+                <button type="submit">add</button>
+            </form>
+            {notes.map(note =>
+                <li key={note.id} onClick={() => toggleImportance(note)}>
+                    {note.content}
+                    <strong> {note.important ? 'important' : ''}</strong>
+                </li>
+            )}
         </div>
     )
 }
