516de43b24ede4762f0739fa55cfddd54e65e902
Grigorii Berezin <grigorii_berezin@epam.com>
2023-03-06 11:48:44 +0100

    Implement routed anecdotes step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import {Link, Route, Routes} from "react-router-dom";
+import {Link, Route, Routes, useMatch} from "react-router-dom";
 
 const Menu = () => {
   const padding = {
@@ -14,11 +14,21 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({anecdote}) => (
+  <div>
+    <p>{anecdote.content}</p>
+  </div>
+)
+
 const AnecdoteList = ({anecdotes}) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}>
+        <Link to={`/anecdotes/${anecdote.id}`}>
+          {anecdote.content}
+        </Link>
+      </li>)}
     </ul>
   </div>
 )
@@ -127,11 +137,15 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match ? anecdotes.find(a => a.id === Number(match.params.id)) : null
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu/>
       <Routes>
+        <Route path={"anecdotes/:id"} element={<Anecdote anecdote={anecdote}/>}/>
         <Route path={"/"} element={<AnecdoteList anecdotes={anecdotes}/>}/>
         <Route path={"/create"} element={<CreateNew addNew={addNew} />}/>
         <Route path={"/about"} element={<About />}/>
